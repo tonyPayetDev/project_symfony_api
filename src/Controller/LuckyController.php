56cb7cf002b40df7a32085e3348f558f59e232bf
tonyPayetDev <tony.payet.professionnel@gmail.com>
2021-01-04 22:29:28 +0300

    first commot

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,23 @@
+<?php
+
+// src/Controller/LuckyController.php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+// ...
+
+class LuckyController extends AbstractController
+{
+    /**
+     * @Route("/lucky/number")
+     */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+        
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
